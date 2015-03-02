d75451674cf81a6f45b7900663f731c5f02c7bfb
amoedo <amoedo@outlook.com>
2015-03-02 17:10:36 +0000

    Added RetrieveMultiple extensions. More UnitTests Example Console App Create a SDK 2015 Project to target the new framework

--- CrmFluentExtensions/CrmFluentExtensions.UnitTest/UnitTestFluentChainACtionWithReturn.cs
+++ CrmFluentExtensions/CrmFluentExtensions.UnitTest/UnitTestFluentChainACtionWithReturn.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Xrm.Sdk;
 using Moq;
+using CrmFluentExtensions;
+using Microsoft.Xrm.Sdk.Query;
+
 
 namespace CrmFluentExtensions.UnitTest
 {
@@ -46,12 +49,12 @@ namespace CrmFluentExtensions.UnitTest
             Guid result = service.CreateFluent(contact)
                 .Retry(1, 2, //2 Retries 1ms apart
                 (e) =>
-                { 
-                    if (e is InvalidOperationException) count++; 
+                {
+                    if (e is InvalidOperationException) count++;
                 },
-                () => 
-                { 
-                    return expected; 
+                () =>
+                {
+                    return expected;
                 })
                 .Do();
 
@@ -77,11 +80,11 @@ namespace CrmFluentExtensions.UnitTest
             var contact = new Entity("contact");
 
             int count = 0;
-                       
+
 
             //Act
             Guid result = service.CreateFluent(contact)
-                .Until(()=>
+                .Until(() =>
                     {
                         return ++count > 3;
                     })
@@ -93,5 +96,209 @@ namespace CrmFluentExtensions.UnitTest
             Assert.AreEqual(4, count);
             Assert.AreEqual(result, expected);
         }
+
+        [TestMethod]
+        public void TestWhile()
+        {
+            //Arrange
+            Guid expected = Guid.Empty;
+
+            var mockService = new Mock<IOrganizationService>();
+
+            mockService.Setup(s => s.Create(It.IsAny<Entity>())).Returns(() => { expected = Guid.NewGuid(); return expected; });
+
+            var service = new FluentOrganizationService(mockService.Object);
+
+            var contact = new Entity("contact");
+
+            int count = 0;
+
+
+            //Act
+            Guid result = service.CreateFluent(contact)
+                .While(() =>
+                {
+                    return ++count <= 3;
+                },
+                (newValue) =>
+                {
+                    Assert.AreEqual(expected, newValue); //Check we are getting the results from the service
+                }
+                )
+                .Do();
+
+            //Assert
+            //Called 3 times, result is as expected and test evaluated 
+            mockService.Verify(s => s.Create(contact), Times.Exactly(3));
+            Assert.AreEqual(4, count);
+            Assert.AreEqual(result, expected); //Check we get the last value
+        }
+
+        [TestMethod]
+        public void TestWhenTrue()
+        {
+            //Arrange
+            Guid expected = Guid.NewGuid();
+
+            var mockService = new Mock<IOrganizationService>();
+
+            mockService.Setup(s => s.Create(It.IsAny<Entity>())).Returns(expected);
+
+            var service = new FluentOrganizationService(mockService.Object);
+
+            var contact = new Entity("contact");
+
+            Guid result = Guid.Empty;
+
+            //Act
+            try
+            {
+
+                result = service.CreateFluent(contact)
+                .WhenTrue(() =>
+                {
+                    return true;
+                })
+                .Do();
+
+                 result = service.CreateFluent(contact)
+                .WhenTrue(() =>
+                {
+                    return false;
+                })
+                .Do();
+
+            }
+            catch (Exception e)
+            {
+                //Expect exception on the second call
+                Assert.IsInstanceOfType(e, typeof(OperationCanceledException));
+            }
+
+
+            //Assert
+            //Called 1 times, result is as expected 
+            mockService.Verify(s => s.Create(contact), Times.Once());
+            Assert.AreEqual(expected, result);
+
+        }
+
+        //Extensions Test
+        [TestMethod]
+        public void TestFirst()
+        {
+            //Arrange
+            
+            var mockService = new Mock<IOrganizationService>();
+
+            Entity expected = new Entity("contact");
+            EntityCollection collection = new EntityCollection();
+            collection.Entities.Add(expected);
+
+            mockService.Setup(s => s.RetrieveMultiple(It.IsAny<QueryBase>())).Returns(collection);
+
+            var service = new FluentOrganizationService(mockService.Object);
+
+            
+            //Act
+            var result = service.RetrieveMultipleFluent(new QueryExpression())
+                .First()
+                .Do();
+
+            //Assert
+            //Called 3 times, exception managed 3 times and result is as expected
+            mockService.Verify(s => s.RetrieveMultiple(It.IsAny<QueryBase>()), Times.Exactly(1));            
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestFirstWithEmpty()
+        {
+            //Arrange
+
+            var mockService = new Mock<IOrganizationService>();
+
+            Entity expected = new Entity("contact");
+            EntityCollection collection = new EntityCollection();            
+
+            mockService.Setup(s => s.RetrieveMultiple(It.IsAny<QueryBase>())).Returns(collection);
+
+            var service = new FluentOrganizationService(mockService.Object);
+
+
+            //Act
+            try
+            {
+                var result = service.RetrieveMultipleFluent(new QueryExpression())
+                    .First()
+                    .Do();
+                Assert.Fail("Exception not thrown");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+               
+            }
+
+            //Assert
+            //Called 3 times, exception managed 3 times and result is as expected
+            mockService.Verify(s => s.RetrieveMultiple(It.IsAny<QueryBase>()), Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void TestFirstOrDefault()
+        {
+            //Arrange
+
+            var mockService = new Mock<IOrganizationService>();
+
+            Entity expected = new Entity("contact");
+            EntityCollection collection = new EntityCollection();
+            collection.Entities.Add(expected);
+
+            mockService.Setup(s => s.RetrieveMultiple(It.IsAny<QueryBase>())).Returns(collection);
+
+            var service = new FluentOrganizationService(mockService.Object);
+
+
+            //Act
+            var result = service.RetrieveMultipleFluent(new QueryExpression())
+                .FirstOrDefault()
+                .Do();
+
+            //Assert
+            //Called 3 times, exception managed 3 times and result is as expected
+            mockService.Verify(s => s.RetrieveMultiple(It.IsAny<QueryBase>()), Times.Exactly(1));
+            Assert.AreEqual(result, expected);
+        }
+
+
+        [TestMethod]
+        public void TestFirstOrDefaultWithEmpty()
+        {
+            //Arrange
+
+            var mockService = new Mock<IOrganizationService>();
+
+            Entity expected = new Entity("contact");
+            EntityCollection collection = new EntityCollection();
+
+            mockService.Setup(s => s.RetrieveMultiple(It.IsAny<QueryBase>())).Returns(collection);
+
+            var service = new FluentOrganizationService(mockService.Object);
+
+
+            //Act
+
+                var result = service.RetrieveMultipleFluent(new QueryExpression())
+                    .FirstOrDefault()
+                    .Do();
+
+
+            //Assert
+            //Called 3 times, exception managed 3 times and result is as expected
+            mockService.Verify(s => s.RetrieveMultiple(It.IsAny<QueryBase>()), Times.Exactly(1));
+            Assert.AreEqual(default(Entity), result);
+        }
+
     }
 }
---
+++ CrmFluentExtensions/CrmFluentExtensions/FluentChainACtionWithReturnExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrmFluentExtensions
+{
+    public static class FluentChainACtionWithReturnExtensions
+    {
+        public static FluentChainActionWithReturn<Entity> First(this FluentChainActionWithReturn<EntityCollection> chain)
+        {
+            var result = new FluentChainActionWithReturn<Entity>(() =>
+            {
+                var collection = chain.Do();
+
+                return collection.Entities[0];
+            });
+
+            return result;
+        }
+
+        public static FluentChainActionWithReturn<Entity> FirstOrDefault(this FluentChainActionWithReturn<EntityCollection> chain)
+        {
+            var result = new FluentChainActionWithReturn<Entity>(() =>
+            {
+                var collection = chain.Do();                
+
+                if (collection.Entities != null && collection.Entities.Count > 0)
+                    return collection.Entities[0];
+                else
+                    return default(Entity);
+            });
+
+            return result;
+        }
+
+    }
+}
