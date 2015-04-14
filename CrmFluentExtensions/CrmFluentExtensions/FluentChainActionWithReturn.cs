133d9faf0983e81196d0a349e4d59cb605ab0c09
Davesmall28 <dave@dsmall.co.uk>
2015-04-14 21:37:13 +0100

    Item is null when item.GetType() is called and would through a NullReferenceException.

--- CrmFluentExtensions/CrmFluentExtensions/FluentChainActionWithReturn.cs
+++ CrmFluentExtensions/CrmFluentExtensions/FluentChainActionWithReturn.cs
@@ -380,9 +380,10 @@ namespace CrmFluentExtensions
                 for (var i = 0; i < values.Length; i++)
                 {
                     var item = values[i];
-                    if (item == null) throw new ArgumentNullException(
-                        string.Format("Parameter of Type {0} at index {1} is null", item.GetType(), i)
-                        );
+                    if (item == null)
+                    {
+                        throw new ArgumentNullException(string.Format("Parameter at index {0} is null", i));
+                    }
                 }
 
                 return action();
@@ -404,11 +405,8 @@ namespace CrmFluentExtensions
                     var item = values[i];
                     if (item == null || item.Equals(default(T)))
                     {
-                        throw new ArgumentNullException(
-                        string.Format("Parameter of Type {0} at index {1} is null or default", item.GetType(), i)
-                        );
+                        throw new ArgumentNullException(string.Format("Parameter at index {0} is null or default", i));
                     }
-
                 }
 
                 return action();
